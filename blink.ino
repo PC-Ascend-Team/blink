53d2db53bd4bc56a97433f7761697ddcf9699a09
jcahal <tutorials@Jonathans-MacBook-Pro-2.local>
2016-05-14 21:19:26 -0700

    final commit... It's alive!

--- blink.ino
+++ blink.ino
@@ -1,9 +1,14 @@
-void setup() {
-  // put your setup code here, to run once:
+int led = 12; // pin for external LED
 
+// the setup routine will run once when the Arduino is powerd on
+void setup() {
+  pinMode(led, OUTPUT); // this sets pin 12 to output signal
 }
 
+// the loop routine will run over and over, forever
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(led, HIGH);   // turn the LED on
+  delay(1000);               // wait for a second
+  digitalWrite(led, LOW);    // turn the LED off
+  delay(1000);               // wait for a second
 }
